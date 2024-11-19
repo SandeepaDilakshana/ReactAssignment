7b3dc5b10be2f489a0c679461da68cc7844d73b7
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-11-19 14:32:44 +0000

    update app.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,5 @@
-//import logo from './logo.svg';
 import './App.css';
-import students from './components/StudentDb.js';
+import { students } from './components/StudentDb'
 
 function App() {
   return (
