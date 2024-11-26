914cbe5ccf87b8c78e344d0e6938d4adce69022f
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-11-26 08:39:42 +0000

    Updating App.js with routing

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
 import { students } from './data/StudentsDb';
 import StudentTable from './components/studentTable';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
     <div className="App">
-    <h1>Student Information Portal</h1>
-    <hr/>
-    <table>
-      <tbody>
-        <tr>
-          <td>
-            <StudentTable students={students}/>
+      <Router>
+        <Routes>
+          <Route path = '/home' element = {<Home/>}/>
+          <Route path = '/about' element = {<AboutUs/>}/>
+          <Route path = '/contact' element = {<ContactUs/>}/>
+        </Routes>
+      </Router>
+      <h1>Student Information Portal</h1>
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <StudentTable students={students} />
             </td>
-        </tr>
+          </tr>
         </tbody>
-    </table>
+      </table>
     </div>
   );
 }
