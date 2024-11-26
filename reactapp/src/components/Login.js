b6b7cf22bdd154bbd70cd17d8584c0570ef478e6
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-11-26 09:57:55 +0000

    update Login.js

--- reactapp/src/components/Login.js
+++ reactapp/src/components/Login.js
@@ -1,17 +1,33 @@
-import { Form, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+
+    const data = [{username:'2020/ICT/21'},
+                  {password:'abc123'}
+    ]
+    const [studentData,setStudentData] = useState({
+        username:"",
+        password:""
+    })
 
     const navigate = useNavigate();
     const authenticate = () => {
         navigate('/dash')
     }
+
     return (
         <div>
             <h1>Login Here</h1>
-            Username :<input type="text" name="username" /><br/>
-            Password :<input type="password" name="password"/><br/>
-            <button onClick={() => authenticate()}>Login</button>
+            <table>
+                <tr>
+                    <td>Username :<input type="text" name="username" /></td>
+                </tr>  
+                <tr>  
+                    <td>Password :<input type="password" name="password" /></td>
+                </tr>
+            </table>
+                <button onClick={() => authenticate()}>Login</button>
         </div>
     );
 }
